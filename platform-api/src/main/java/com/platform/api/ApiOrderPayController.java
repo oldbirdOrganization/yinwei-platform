1eb357a8e523460fc3407a70ad4fc28800fb18e9
xialiangju <642599645@qq.com>
2019-08-18 00:00:45 +0800

    no message

--- platform-api/src/main/java/com/platform/api/ApiOrderPayController.java
+++ platform-api/src/main/java/com/platform/api/ApiOrderPayController.java
@@ -104,7 +104,7 @@ public class ApiOrderPayController extends ApiBaseAction {
             // 交易类型APP
             parame.put("trade_type", ResourceUtil.getConfigByName("wx.tradeType"));
             parame.put("spbill_create_ip", getClientIp());
-            //parame.put("openid", loginUser.getWeixin_openid());
+            parame.put("openid", loginUser.getWeixin_openid());
             String sign = WechatUtil.arraySign(parame, ResourceUtil.getConfigByName("wx.paySignKey"));
             // 数字签证
             parame.put("sign", sign);
